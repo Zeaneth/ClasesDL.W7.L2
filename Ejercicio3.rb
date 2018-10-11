79925f4eef8f1b24c111511b8b086a2dd1ba24b1
Zamiz <zamiztobar@gmail.com>
2018-10-11 20:31:29 -0300

    +Ejercicio3.rb

---
+++ Ejercicio3.rb
@@ -0,0 +1,20 @@
+#1. Crear un menú que permita registrar la información de los alumnos del curso.
+#
+#2. Opción 1: Permite ingresar los datos de una persona:
+#    - Nombre
+#    - Edad
+#    - Comuna
+#    - Género
+#
+#3. Opción 2: Permite editar los datos de la persona.
+#4. Opción 3: Permite eliminar una persona.
+#5. Opción 4: Muestra la cantidad de personas ingresadas.
+#6. Opción 5: Muestra las comunas de todas las personas.
+#
+#	*hint:* Generar un array con las comunas, eliminar repeticiones. Considerar que pueden haber nombres escritos con mayúscula y minúscula.
+#
+#5. Opción 6: Muestra una lista con todas las personas que tengan entre 20 y 25 años.
+#6. Opción 7: Muestra la suma de las edades de todas las personas.
+#7. Opción 8: Muestra el promedio de las edades del grupo.
+#8. Opción 9: Muestra dos listas de personas, una por cada género.
+#9. El menú debe repetirse hasta que el usuario ingrese la opción 10 (salir).
