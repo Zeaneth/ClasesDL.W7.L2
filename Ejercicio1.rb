084918f8cde04b498196d6dce38df09b451a9627
Zamiz <zamiztobar@gmail.com>
2018-10-11 19:37:51 -0300

    First commit

---
+++ Ejercicio1.rb
@@ -0,0 +1,19 @@
+#Dado el array:
+#
+#~~~ruby
+# a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+#~~~
+#
+#1. Utilizando *map* generar un nuevo arreglo con cada valor aumentado en 1.
+#2. Utilizando *map* generar un nuevo arreglo que contenga todos los valores convertidos a *float*.
+#3. Utilizando *map* generar un nuevo arreglo que contenga todos los valores convertidos a *string*.
+#4. Utilizando *reject* descartar todos los elementos <u>menores</u> a 5 en el array.
+#5. Utilizando *select* descartar todos los elementos <u>mayores</u> a 5 en el array.
+#6. Utilizando *inject* obtener la suma de todos los elementos del array.
+#7. Utilizando *group_by* agrupar todos los números por paridad (si son pares, es un grupos, si son impares es otro grupo).
+#8. Utilizando *group_by* agrupar todos los números mayores y menores que 6.
+
+a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+
+a.map { |x| x + 1 }
+puts a.map
